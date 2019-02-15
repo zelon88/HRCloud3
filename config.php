3f79a87d7659b40c57e97360ad573f2401e6b479
Justin Grimes <zelon88@gmail.com>
2019-02-15 11:25:28 -0500

    Update config.php

--- config.php
+++ config.php
@@ -10,18 +10,18 @@ $UpdateSources = array('https://github.com/zelon88/Diablo-Engine/');
 // / Security Salts.
 // / Set AT LEAST 4 array elements to use for authenticating operations that require additional security. 
 // / Add additional array elenents will be used where possible, but the first 4 are required. 
-$Salts = array('fgdsfg!sdhafbde3i85_+#$@%<G345234381234120', '2lw12564165fgdasfsdf585&^e4f1e3djtjthfnb erfsdaf', 
- '><<>?#@$@%$f%^$#$#!$$#@!DFASF #$FERG#$F34f3F$42F34f$f4', '5683bnfrbnd7uh78r34hp9rh437r8g34378734ryh37489ryh347r9');
+$Salts = array('fgdsfg!sdhafbdegfgfdsg3i85_+#$@%<gdfsG345234381234120', '2lw12564165fgdgdfgsdasfsdf585&^e4f1e3djtjthfnb erfsdaf', 
+ '><<>?#@$@%$f%^$#$#!$$#@!fdgsdfDFASF #$FERG#$F34f3F$42F34f$f4', '5683bnfrbnd7uh78r34hp9rhfgsgf437r8g34378734ryh37489ryh347r9');
 
 // / Library Definitions.
 // / Libraries are treated as objects. They are defined in the following arrays.
 // / Arrays are formatted as  $Libraries['LIBRARY_NAME', "ENABLED/DISABLED(bool)", '/path/to/library/directory']
 // / Admins can add their own custom libraries by simply copy/pasting an existing entry and being cautious of the ending '('.
 $Libraries = array(
- array('MOVIES', "TRUE", '/media/justin/Media/Media/Movies'),
- array('MUSIC', "TRUE", '/media/justin/Media/Media/Music'),
- array('SHOWS', "TRUE", '/media/justin/Media/Media/Shows'),
- array('CHANNELS', "FALSE", '/media/justin/Media/Media/YouTube Subscriptions'),
+ array('MOVIES', "TRUE", '/media/user/Media/Media/Movies'),
+ array('MUSIC', "TRUE", '/media/user/Media/Media/Music'),
+ array('SHOWS', "TRUE", '/media/user/Media/Media/Shows'),
+ array('CHANNELS', "FALSE", '/media/user/Media/Media/YouTube Subscriptions'),
  array('DRIVE', "FALSE", ''),
  array('STREAMS', "FALSE", ''),
  array('IMAGES', "FALSE", ''),
@@ -35,8 +35,8 @@ $Libraries = array(
 // / Users added through the GUI after initial setup are contained in the cache.
 // / Arrays are formatted as  $Users['USER_ID', 'USER_NAME', 'USER_EMAIL', 'SHA-256_HASHED_PASSWORD', "ADMIN_YES/NO(bool)", "LAST_SESION_ID"]
 $Users = array(
- array('1', 'zelon88', 'test@gmail.com', 'testpassword', "TRUE", ""),
- array('2', 'Nikki', 'test@gmail.com', 'password', "FALSE", "") );
+ array('1', 'zelon88', 'test@gmail.com', 'testpasswordyness', "TRUE", ""),
+ array('2', 'Nikki', 'test@gmail.com', 'passwordyish', "FALSE", "") );
 
 // / If the passwords for the config.php users listed above are in plain-speech they will need to be hashed and stored in the cache
 // / before they are valid. Set the  $HashConfigPasswords  to  TRUE  if the passwords above are plainspeech. Set to  FALSE  if they
