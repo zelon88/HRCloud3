4f5705199eb77f1a82c59308d2b670adba5ed682
Justin Grimes <zelon88@gmail.com>
2022-02-17 02:42:28 -0500

    v0.7.7 - Working on maintaining sessions.
    -Working on maintaining sessions with UI elements.

--- Resources/Themes/DEFAULT/login.php
+++ Resources/Themes/DEFAULT/login.php
@@ -9,30 +9,12 @@ Licensed Under GNU GPLv3
 https://www.gnu.org/licenses/gpl-3.0.html
 
 Author: Justin Grimes
-Date: 8/13/2019
+Date: 2/16/2022
 <3 Open-Source
 
 The Login file provides UI elements enabling users to gain access to their account.
 */
 
-
-
-
-
-
-
-
-
-// / This page needs to collect the username and submit it to the core.php file.
-// / Required POSTS 
-// /  Username 
-// /  RequestTokens
-
-// /  Country?
-
-// / The server will respond with a fresh ClientToken
-// / Once the user has obtained a ClientToken they can submit the client token, username, and password to the core.
-// / The core will validate the inputs and either allow or deny the login request.
 ?> 
   <div id="wholePageLogin1" class="modal2">
     <div id="wholePageLogin1" class="center" style="width:75%; height:50%;">
