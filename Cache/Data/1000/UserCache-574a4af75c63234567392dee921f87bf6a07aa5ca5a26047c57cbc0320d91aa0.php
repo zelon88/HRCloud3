2cae2dee7849b1dcb8ab6f4907f8449c8e72482e
Justin Grimes <zelon88@gmail.com>
2022-04-10 02:06:14 -0400

    v0.9.5 - Tidying up existing processes, fixing bugs.
    -Fixed a bug where the Create Account form would be unreachable after creating one account.
    -Added red outline to most form fields when they are empty and the form is submitted.
    -Added dynamic "require" attribute to certain text fields to enable default browser functionality when these are blank.
      -Will eventually have to go through and consolidate the JS in login-caller.js.
      -Maybe move to a separate file & just leave AJAX in login-caller.js?
    -Added several "cancel" functions to Login-Caller.js for resetting form fields when a process is abandoned by the user.
    -Fixed new user account User ID collisions.
    -Fixed a bug in the way the PostConfigUser cache file was being written.
      -The array was malformed.
    -Existing cache files will need to be replaced.
    -Existing user accounts created through the UI must be discarded.
      -It is a good thing this is a very early prototype and there aren't any other features yet.
    -Fixed a bug where the user cache was not being put into a subdirectory.
      -The UserID was not being properly defined during existing sessions (continued sessions).
      -Now the UserID is properly defined in verifyGlobals() by verifySession().
      -An extra check was added to verifyUserEnvironment() to ensure that the UserID is valid before any filesystem operations are performed.
      -Added ERROR 37!
        -The UserID is not valid!
    -Started working on Forgot Password Modal.
    -Added functions for checking/unchecking checkboxes to HRC3-Lib.js.

--- Cache/Data/1000/UserCache-574a4af75c63234567392dee921f87bf6a07aa5ca5a26047c57cbc0320d91aa0.php
+++
@@ -1,2 +0,0 @@
-<?php
-$userCacheData = array('FRIENDS'=>'', 'BLOCKED'=>'', 'COLOR'=>'BLUE', 'FONT'=>'ARIAL', 'TIMEZONE'=>'America/New_York', 'DISPLAYNAME'=>'Commander', 'TIPS'=>'ENABLED', 'THEME'=>'DEFAULT', 'HRAI'=>'ENABLED', 'HRAIAUDIO'=>'ENABLED', 'LANDINGPAGE'=>'DEFAULT', 'STAYLOGGEDIN'=>'ENABLED');
--- Cache/Data/7/UserCache-06f9e28472d6c00e778cb337a850a4eae75c23428db894f034df8489191784cd.php
+++
@@ -1,3 +0,0 @@
-<?php
-$userCacheData = array('FRIENDS'=>'', 'BLOCKED'=>'', 'COLOR'=>'BLUE', 'FONT'=>'ARIAL', 'TIMEZONE'=>'America/New_York', 'DISPLAYNAME'=>'Commander', 'TIPS'=>'ENABLED', 'THEME'=>'DEFAULT', 'HRAI'=>'ENABLED', 'HRAIAUDIO'=>'ENABLED', 'LANDINGPAGE'=>'DEFAULT', 'STAYLOGGEDIN'=>'ENABLED');
-$userCacheData = array('COLOR'=>'BLUE', 'FONT'=>'ARIAL', 'TIMEZONE'=>'America/New_York', 'DISPLAYNAME'=>'Commander', 'TIPS'=>'ENABLED', 'THEME'=>'DEFAULT', 'HRAI'=>'ENABLED', 'HRAIAUDIO'=>'ENABLED', 'LANDINGPAGE'=>'DEFAULT', 'STAYLOGGEDIN'=>'ENABLED');
